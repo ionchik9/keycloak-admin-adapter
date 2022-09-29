c8ca6dbea0f228ec0503d032b3bd082c609bd7a3
Ion Ionets <ion.ionets@agenatech.com>
2022-09-29 18:04:51 +0800

    api corrections

--- src/main/java/com/agenatech/keycloakadminadapter/model/payload/TherapistProfile.java
+++ src/main/java/com/agenatech/keycloakadminadapter/model/payload/TherapistProfile.java
@@ -3,7 +3,6 @@ package com.agenatech.keycloakadminadapter.model.payload;
 
 import com.agenatech.keycloakadminadapter.model.payload.enums.Language;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -37,7 +36,6 @@ public class TherapistProfile {
     private List<String> states;
 
     @Schema( accessMode = Schema.AccessMode.READ_ONLY)
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String calendarInviteUrl;
 
 }
